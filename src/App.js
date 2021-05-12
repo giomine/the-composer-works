150f3080b32a300770fdcfb79acf4d2edf851901
Giorgia Mineo <giorgiamineo@Giorgias-MacBook-Air.local>
2021-05-12 16:23:36 +0100

    Add background image

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import "./global.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import LandingPage from "./components/LandingPage";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 
 function App() {
   return (
     <Router>
-      <Navbar />
+      <LandingPage />
       
     </Router>
   );
---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+function LandingPage(){
+    return(
+        <div className="landingPageBG">
+            <Navbar />
+        </div>
+    )
+}
+
+export default LandingPage;
