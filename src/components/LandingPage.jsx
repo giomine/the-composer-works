c4a3866a3695abbc360c46d17e0566a9761c1364
Giorgia Mineo <giorgiamineo@Giorgias-MacBook-Air.local>
2021-05-12 17:39:39 +0100

    Add hero text section

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -3,8 +3,16 @@ import Navbar from "./Navbar";
 
 function LandingPage(){
     return(
-        <div className="landingPageBG">
-            <Navbar />
+        <div className="page">
+            <div className="landingPageBG">
+                <Navbar />
+            </div>
+
+            <div className="hero-text">
+                <h1>We are an elite group of <span className="bold">film</span> and <span className="bold">tv composers</span>.</h1>
+                <p>FOR CLIENTS LIKE THESE</p>
+                <img src={process.env.PUBLIC_URL + "./assets/IconsLogo/Triangle.svg"} alt="logo" />
+            </div>
         </div>
     )
 }
