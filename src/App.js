95411b2b2193428a9016bbd23b6fc55e044104e4
Giorgia Mineo <giorgiamineo@Giorgias-MacBook-Air.local>
2021-05-13 12:19:10 +0100

    Move Navbar component to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./global.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
+import Navbar from "./components/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <LandingPage />
       
     </Router>
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import Navbar from "./Navbar";
 
 function LandingPage(){
     return(
         <div className="page">
             <div className="landingPageBG">
-                <Navbar />
             </div>
 
             <div className="hero-text">
